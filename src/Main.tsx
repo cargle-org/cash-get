34cea4eecb7679d61a2a54b2d9652c00079f5b3e
Olaolu Johnson <johnsonolaolu@gmail.com>
2023-07-07 06:09:57 +0100

    initial setup

---
+++ src/Main.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import IndexRouter from "./router/index.router";
+
+const Main = () => {
+  return <IndexRouter />;
+};
+
+export default Main;
---
+++ src/router/index.router.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const IndexRouter = () => {
+  return <></>;
+};
+
+export default IndexRouter;
